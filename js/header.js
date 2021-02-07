a538c1a1ab34e73a65f856e6e0e746fda739e2a7
Volodymyr Ryba <vovaryba@gmail.com>
2021-02-07 21:27:21 +0200

    Копия 7 ДЗ

---
+++ js/header.js
@@ -0,0 +1,7 @@
+(() => {
+	const { height: pageHeaderHeight } = document
+		.querySelector(".page-header")
+		.getBoundingClientRect();
+	
+	document.body.style.paddingTop = `${pageHeaderHeight}px`;
+})();
